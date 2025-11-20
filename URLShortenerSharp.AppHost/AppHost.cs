d63ee0988a420ea56d4542243fbdd6451f3787a2
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2025-11-20 11:07:33 +0100

    add docker compose in aspire

--- URLShortenerSharp.AppHost/AppHost.cs
+++ URLShortenerSharp.AppHost/AppHost.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDockerComposeEnvironment("URLShortSharpDockerEnv");
+
 builder.AddProject<Projects.URLShortenerSharp>("Main");
 
 builder.Build().Run();
